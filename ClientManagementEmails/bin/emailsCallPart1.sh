37b65dbd1ef2586cb94bdb8765e89ca4d48b41fa
Frank Ryan <frankryan@Franks-MacBook-Pro.local>
2017-05-09 10:43:36 +0100

    tidying

--- ClientManagementEmails/bin/emailsCallPart1.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-php /home/visar/CRMTool/ClientManagementEmails/bin/downloadEmailsCSV.php
-ls /home/visar/CRMTool/ClientManagementEmails/credentials | parallel -j0 php /home/visar/CRMTool/ClientManagementEmails/bin/downloadEmails.php
-
--- ClientManagementEmails/bin/emailsCallPart2.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-php /home/visar/CRMTool/ClientManagementEmails/bin/combineOutputs.php
-php /home/visar/CRMTool/ClientManagementEmails/bin/uploadEmailsCSV.php
-
